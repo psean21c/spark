ddd8d1390911d85dbf51575bced82d2f408c785b
simonpark <sungmin.park@zafin.com>
2021-05-03 11:32:31 -0400

    refactoring useEffect

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,45 +1,17 @@
-import {useState, useEffect} from 'react';
+// import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
+
 const Home = () => {
+    const url = "./students"
+    const {data: blogs,isPending, error} = useFetch(url);
 
-    // useState()
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    // useEffect()
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('/students')
-            .then(res => {
-                if (!res.ok) {
-                    throw Error('Not able to fetch data from server');
-                }
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch(err => {
-                setIsPending(false);
-                setError(err.message);
-            });    
-        },1000);
-    },[])
-
-    // this method will be called from ./BolgList
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
 
     return ( 
         <div className="home">
             {error && <div>{error}</div>}
             {isPending && <div>Loading ...</div>}
-            {blogs && <BlogList blogs={blogs} title="Connected Blogs !!" handleDelete ={handleDelete}/>}
+            {blogs && <BlogList blogs={blogs} title="Connected Blogs !!"/>}
         </div>
      );
 }
---
+++ src/useFetch.js
@@ -0,0 +1,37 @@
+import {useState, useEffect} from 'react';
+
+const useFetch = (url) => {
+
+        // useState()
+        const [data, setData] = useState(null)
+        const [isPending, setIsPending] = useState(true);
+        const [error, setError] = useState(null);
+    
+        // useEffect()
+        useEffect(() => {
+            setTimeout(() => {
+                fetch(url)
+                // fetch('/students')
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error('Not able to fetch data from server');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setData(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                });    
+            },1000);
+        },[url]);
+
+        return {data, isPending, error}
+    
+}
+
+export default useFetch;
